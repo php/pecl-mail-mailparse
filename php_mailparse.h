1911ea257c27344e2f22367e8055e779dfd314e0
Brian Shire <shire@php.net>
2009-03-03 22:06:59 +0000

    2.1.5 release

--- php_mailparse.h
+++ php_mailparse.h
@@ -25,7 +25,7 @@
 extern zend_module_entry mailparse_module_entry;
 #define phpext_mailparse_ptr &mailparse_module_entry
 
-#define PHP_MAILPARSE_VERSION "2.1.5-dev"
+#define PHP_MAILPARSE_VERSION "2.1.5"
 
 #ifdef PHP_WIN32
 #define PHP_MAILPARSE_API __declspec(dllexport)
